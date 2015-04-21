aed05f6efd4c592c7a2fa33cdb2cc8755a9a1983
HaiBo Song <hason@microsoft.com>
2015-04-21 11:45:47 +0800

    First employee data model

---
+++ EIMS/DAL/EIMS.DataModel/EIMSDataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace EIMS.DataModel
+{
+    public class EIMSDataContext : DbContext
+    {
+        public EIMSDataContext()
+            : base("EIMSDataContext")
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EIMS/DAL/EIMS.DataModel/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EIMS.DataModel
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Gender { get; set; }
+
+        public DateTimeOffset Birthday { get; set; }
+
+        public string Phone { get; set; }
+
+        public string MobilePhone { get; set; }
+
+        public DateTimeOffset OnboardDate { get; set; }
+
+        public string Title { get; set; }
+
+        public int ManagerId { get; set; }
+
+        public virtual Employee Manager { get; set; }
+
+        public string Comments { get; set; }
+    }
+}
